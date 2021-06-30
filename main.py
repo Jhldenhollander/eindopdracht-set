ebaad2956e86454faaceb985ac0ffd9c1a75eebc
Jhldenhollander <81688426+Jhldenhollander@users.noreply.github.com>
2021-06-30 14:00:49 +0200

    Add files via upload
    kappen luuk

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def eigenschappen_vergelijken(stapel):
             sets.append(combinatie[i])
     return sets
 
-#dikke yeet
+#dikke yeet 2,0
 print(str(eigenschappen_vergelijken(table())))
 # klasse schrijven die vgl van eigenschappen ondersteundt
 # algoritme dat sets kan opsporen
